12f930a639fbdadf488a72c4c319a30affff9191
thiagocbarreto <32571922+thiagocbarreto@users.noreply.github.com>
2022-03-05 23:37:10 -0300

    feat: section 1, part 4

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.8.0; // ! Be sure that the compiler version is the same in ha
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint256 totalWaves;
+  
   constructor() {
     console.log("My first contract!!");
+  }
+
+  function wave() public { // The `public`keyword makes the function available to be called
+                           // on the blockchain.
+    totalWaves += 1;
+    console.log("%s has waved!", msg.sender); // `msg.sender` is the wallet address of the
+                                              // person who called the function
+  }
+
+  function getTotalWaves() public view returns (uint256) {
+    console.log("We have %d total waves!", totalWaves);
+    return totalWaves;
   }
 }
--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -1,23 +1,44 @@
 const main = async () => {
-  // 1. Compile our contract and generate the necessary files we need to work with our
-  //    contract under the `artifacts` directory.
+  // In order to deploy something to the blockchain, we need to have a wallet address!
+  // Hardhat does this for us magically in the background, but here I grabbed the wallet
+  // address of contract owner and I also grabbed a random wallet address and called it `randomPerson`.
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+
+  // Compile our contract and generate the necessary files we need to work with our
+  // contract under the `artifacts` directory.
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
 
-  //  2. Hardhat will create a local Ethereum network for us, but just for this contract.
-  //     Then, after the script compĺetes it'll destroy that local network. Every time you
-  //     run the contract, it'll be a fresh blockchain.
+  // Hardhat will create a local Ethereum network for us, but just for this contract.
+  // Then, after the script compĺetes it'll destroy that local network. Every time you
+  // run the contract, it'll be a fresh blockchain.
   const waveContract = await waveContractFactory.deploy();
 
-  // 3. Wait until our contract is officially deployed to our local blockchain. Our `constructor`
-  //    runs when we actually deploy.
+  // Wait until our contract is officially deployed to our local blockchain. Our `constructor`
+  // runs when we actually deploy.
   await waveContract.deployed();
 
-  // 4. Once it's deployed `waveContract.address` will basically give us the address of the
-  //    deployed contract. This address is how we can find our contract on the blockchain.
-  //    There are millions of contracts on the actual blockchain. So, this address gives us
-  //    easy access to the contract we're interested in working with. This will be more
-  //    important a bit later once we deploy to a real Ethereum network.
+  // Once it's deployed `waveContract.address` will basically give us the address of the
+  // deployed contract. This address is how we can find our contract on the blockchain.
+  // There are millions of contracts on the actual blockchain. So, this address gives us
+  // easy access to the contract we're interested in working with. This will be more
+  // important a bit later once we deploy to a real Ethereum network.
   console.log("Contract deployed to: ", waveContract.address);
+
+  // Just to see the address of the person deploying our contract.
+  console.log("Contract deployed by: ", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+  
+  waveCount = await waveContract.getTotalWaves();
 }
 
 const runMain = async () => {
