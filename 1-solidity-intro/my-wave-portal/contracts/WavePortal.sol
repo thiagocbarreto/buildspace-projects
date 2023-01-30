25fabf652a80715221a3e8ba62d97c1780cd9d37
thiagocbarreto <32571922+thiagocbarreto@users.noreply.github.com>
2023-01-30 09:31:42 -0300

    feat(p1): cooldowns to prevent spammers

--- 1-solidity-intro/my-wave-portal/contracts/WavePortal.sol
+++ 1-solidity-intro/my-wave-portal/contracts/WavePortal.sol
@@ -1,15 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves;
-
-    /*
-     * We will be using this below to help generate a random number
-     */
     uint256 private seed;
 
     event NewWave(address indexed from, uint256 timestamp, string message);
@@ -22,6 +18,12 @@ contract WavePortal {
 
     Wave[] waves;
 
+    /*
+     * This is an address => uint mapping, meaning I can associate an address with a number!
+     * In this case, I'll be storing the address with the last time the user waved at us.
+     */
+    mapping(address => uint256) public lastWavedAt;
+
     constructor() payable {
         console.log("We have been constructed!");
         /*
@@ -31,6 +33,19 @@ contract WavePortal {
     }
 
     function wave(string memory _message) public {
+        /*
+         * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
+         */
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15m"
+        );
+
+        /*
+         * Update the current timestamp we have for the user
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
 
@@ -41,17 +56,9 @@ contract WavePortal {
          */
         seed = (block.difficulty + block.timestamp + seed) % 100;
 
-        console.log("Random # generated: %d", seed);
-
-        /*
-         * Give a 50% chance that the user wins the prize.
-         */
         if (seed <= 50) {
             console.log("%s won!", msg.sender);
 
-            /*
-             * The same code we had before to send the prize.
-             */
             uint256 prizeAmount = 0.0001 ether;
             require(
                 prizeAmount <= address(this).balance,
